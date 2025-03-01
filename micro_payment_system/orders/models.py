554e3e4a972ae816bbe8d4fc4832e1e3e45ec4a0
Danila <dangun711@gmail.com>
2025-03-01 20:37:51 +0300

    CREATE APP orders AND CREATE MODEL Orders
    model have colum items to many to many relationships items
    
    model have column all_price which is calculated dynamically when calling
    the get_all_price method
    
    model have methods __conversion_dirham_to_dollar - for conversion
    dirham to dollar
    
    model have method get_all_price - to sum all price items

---
+++ micro_payment_system/orders/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+from items.models import Items
+# Create your models here.
+
+class Orders(models.Model):
+
+    items = models.ManyToManyField(Items)
+
+    all_price = models.DecimalField(max_digits = 10, decimal_places = 2, default = 0, db_default = 0)
+
+    def __conversion_dirham_to_dollar(self, price):
+        return price * 3.67
+
+    def get_all_price(self):
+        all_price = 0
+        all_item = self.items.all()
+        for item in all_item:
+
+            if item.currency == Items.CHOICES_AED:
+                item.price = self.__conversion_dirham_to_dollar(float(item.price))
+
+            all_price += float(item.price)
+        self.all_price = all_price
+        return self.all_price
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+
+        return reverse('one_order', kwargs={'order_id':self.pk})
