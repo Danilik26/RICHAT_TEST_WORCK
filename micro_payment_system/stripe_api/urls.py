e9710ea453a10891338b7dbf921aee44c707ac05
Danila <dangun711@gmail.com>
2025-03-02 09:43:30 +0300

    CREATE URLS FOR PAYMENT PROCESSING

--- micro_payment_system/stripe_api/urls.py
+++ micro_payment_system/stripe_api/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("<int:item_id>", views.MakeStripeSessionForOneItem.as_view()),
-    path("order/by/<int:order_id>", views.MakeStripeSessionForOrder.as_view(),)
+    path("order/by/<int:order_id>", views.MakeStripeSessionForOrder.as_view(),),
+    path("successful_purchase", views.successful_purchase, name = 'successful_purchase'),
+    path("cancel_purchase", views.cancel_purchase, name = 'cancel_purchase'),
 ]
