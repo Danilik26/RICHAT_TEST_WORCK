79da4fc8e454eb53165d56ff50b68b7d00bfafc0
Danila <dangun711@gmail.com>
2025-03-02 16:41:44 +0300

    FIX BAG
    in form in orders CHIOCES_ID_ORDERS have bag that order ids were executed before migrations were applied
    
    I solved everything by simply wrapping it in try, except

--- micro_payment_system/orders/forms.py
+++ micro_payment_system/orders/forms.py
@@ -2,10 +2,15 @@ from django import forms
 
 from .models import Orders
 
+from django.db.utils import ProgrammingError
+
 
 class AddItemToOrderForm(forms.Form):
 
-    CHIOCES_ID_ORDERS = {f'{x.id}':x.pk for x in Orders.objects.all()}
+    try:
+        CHIOCES_ID_ORDERS = {f'{x.id}':x.pk for x in Orders.objects.all()}
+    except ProgrammingError:
+        CHIOCES_ID_ORDERS = {}
 
     id_order = forms.ChoiceField(choices = CHIOCES_ID_ORDERS)
     
